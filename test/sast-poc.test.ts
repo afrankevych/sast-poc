2c2cf8317c3e7838238b4a9ddf4545324528cd27
Andriy Frankevych <andriy.frankevych@docebo.com>
2023-05-11 16:06:50 +0200

    initial setup

---
+++ test/sast-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SastPoc from '../lib/sast-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sast-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SastPoc.SastPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
