35e5242cbcf1cf6eae64795fe55753118fed38a5
Andriy Frankevych <andriy.frankevych@docebo.com>
2023-05-15 18:10:27 +0200

    add orca fs scan

--- bin/scan.sh
+++ bin/scan.sh
@@ -2,4 +2,5 @@
 
 # We don't know if we can use orca security for STAT in CI, but for demonstration purpose we'll use the audit script
 #orca-cli -p sast-poc --api-token <API_TOKEN>  --config ./orca.yml iac scan --path lib
+#orca-cli -p sast-poc --api-token <API_TOKEN>  --config ./orca.yml fs scan --path src
 npm audit
