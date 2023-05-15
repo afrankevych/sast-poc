95a2ed91373f1399da6f7d2f5afa6cb6ed38c210
Andriy Frankevych <andriy.frankevych@docebo.com>
2023-05-15 17:53:06 +0200

    debugging scan script

--- bin/scan.sh
+++ bin/scan.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -l
 
-echo "hello world"
+# We don't know if we can use orca security for STAT in CI, but for demonstration purpose we'll use the audit script
+#orca-cli -p sast-poc --api-token <API_TOKEN>  --config ./bin/scan.sh iac scan --path lib
+npm audit
