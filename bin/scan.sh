857285deb4f5f290f604a19586e1460b71c3e0da
Andriy Frankevych <andriy.frankevych@docebo.com>
2023-05-15 18:07:36 +0200

    format action scan script

--- bin/scan.sh
+++ bin/scan.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 
-orca-cli
-
 # We don't know if we can use orca security for STAT in CI, but for demonstration purpose we'll use the audit script
 #orca-cli -p sast-poc --api-token <API_TOKEN>  --config ./orca.yml iac scan --path lib
-
 npm audit
